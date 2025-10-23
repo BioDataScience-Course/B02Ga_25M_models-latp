3f8f442fc8a5a006f944e7083e4e71cddd575deb
PierreMartinL <220219@umons.ac.be>
2025-10-23 10:32:19 +0000

    Importation de donéées

--- R/import.R
+++ R/import.R
@@ -1,5 +1,16 @@
 # Importation et remaniement des données
 
+# Ceci est nécessaire pour les tests SDD, ne pas utiliser dans un "vrai" projet
+if (!"tools:tests" %in% search())
+  source(here::here("tests/tools_tests.R"), attach(NULL, name = "tools:tests"))
+
+# Configure Knitr to use AGG as graphic device
+knitr::opts_chunk$set(dev = "ragg_png")
+
+# Configuration de l'environnement
+SciViews::R("model", lang = "fr")
+
+sea_snake <- read("data/Aipysurine_sex_dimorphism_for_Dryad2.xlsx") 
 
 # Etape 1 : Importation des données brutes --------------------------------
 
