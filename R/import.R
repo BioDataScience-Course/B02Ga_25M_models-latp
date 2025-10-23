dc958f4c28a91eab0d394995394c3ad7354c15e5
PierreMartinL <220219@umons.ac.be>
2025-10-23 17:47:00 +0000

    Import prémaché

--- R/import.R
+++ R/import.R
@@ -18,30 +18,58 @@ sea_snake <- read("data/Aipysurine_sex_dimorphism_for_Dryad2.xlsx")
 
 # Etape 2 : Description brève des données ---------------------------------
 
-#* A. Genus name
-#* B. species name
-#* C. Museum in which specimens are stored (Australian Museum or Museums and Art Galleries of the Northern Territory [=MAGNT])
-#* D. Museum registration number
-#* E. Snout-vent length (cm)
-#* F. Tail length (cm)
-#* G. Tail maximum width (cm)
-#* H. Head length (mm) measured along jawline
-#* I. Head width (mm) at widest point
-#* J. Body width at midbody (cm)
-#* K. Eye diameter (mm)
-#* L. Sex (f = female, m = male)
-#* M. Maturity (a = adult, j = juvenile)
+# Genus = Genus name
+# species = species name
+# Museum = Museum in which specimens are stored (Australian Museum or Museums and Art Galleries of the Northern Territory [=MAGNT])
+# Museum Rego No = Museum registration number
+# SVL = Snout-vent length (cm)
+# Tail length = Tail length (cm)
+# Tail width (max) = Tail maximum width (cm)
+# Head length = Head length (mm) measured along jawline
+# Head width = Head width (mm) at widest point
+# Body width = Body width at midbody (cm)
+# Eye diam. = Eye diameter (mm)
+# Sex = (f = female, m = male)
+# Maturity = (a = adult, j = juvenile)
 
 #In all columns, "0" = missing data due to damaged or incomplete specimen
 
 # Etape 3 : Nettoyage des données  ----------------------------------------
 sea_snake %>.%
 sfilter(., 
-  `Tail length (cm)`> 0, `tail width (max)`> 0, `head length (mm)`> 0, `head width (mm)`> 0, `eye diam. (mm)`> 0 ) -> sea_snake_propre
+  `genus` != "Aipysu+C370rus" , `Tail length (cm)`> 0, `tail width (max)`> 0, `head length (mm)`> 0, `head width (mm)`> 0, `eye diam. (mm)`> 0 ) -> sea_snake_propre
 
 # Etape 4 : Ajout des labels et des unités --------------------------------
 
-
+sea_snake_propre <- labelise(sea_snake_propre,
+  label = list(
+    `genus` = "genre",
+    `species` = "espèce",
+    `MUSEUM` = "musée",
+    `Museum Rego No` = "id du musée",
+    `SVL (cm)` = "longueur museau-cloaque",
+    `Tail length (cm)` = "longueur de la queue",
+    `tail width (max)` = "largeur de la queue",
+    `head length (mm)` = "longueur de la tête",
+    `body width (cm)` = "largeur du corps",
+    `eye diam. (mm)`= "diamètre de l'œuil",
+    `Sex` = "sexe",
+    `Adult?`= "adulte ?"
+      ),
+  units = list(
+    `genus` = NA,
+    `species`= NA,
+    `MUSEUM` = NA,
+    `Museum Rego No` = NA,
+    `SVL (cm)` = "cm",
+    `Tail length (cm)` = "cm",
+    `tail width (max)` = "max",
+    `head length (mm)` = "mm",
+    `body width (cm)` = "cm",
+    `eye diam. (mm)`= "mm",
+    `Sex` = NA,
+    `Adult?`= NA
+  ))
 
 # Etape 5 : Sauvegarde locale des données retravaillées -------------------
 
